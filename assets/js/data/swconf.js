50a40839d47a54a7206b669d23c3413623002d73
xiangzheng666 <xiangzheng666@users.noreply.github.com>
2025-05-25 05:54:34 +0000

    deploy: 1e30f432e21016b4448f16351e9e71b7a591cd33

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748073643',resources: [
+    cacheName: 'chirpy-1748152472',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
